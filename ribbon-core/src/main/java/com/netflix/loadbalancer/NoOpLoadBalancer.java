1e333eb3485f7749b9f83f50e04b02f74527b855
Sudhir Tonse <stonse@gmail.com>
2013-01-22 22:54:14 -0800

    Added Copyright Headers to all files. Consolidated Imports

--- ribbon-core/src/main/java/com/netflix/loadbalancer/NoOpLoadBalancer.java
+++ ribbon-core/src/main/java/com/netflix/loadbalancer/NoOpLoadBalancer.java
@@ -1,3 +1,20 @@
+/*
+*
+* Copyright 2013 Netflix, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 package com.netflix.loadbalancer;
 
 import java.util.Collections;
@@ -5,8 +22,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.netflix.niws.client.IClientConfig;
 
 /**
  * A noOp Loadbalancer
--- ribbon-core/src/main/java/com/netflix/niws/client/ZoneAwareNIWSDiscoveryLoadBalancer.java
+++ ribbon-core/src/main/java/com/netflix/niws/client/ZoneAwareNIWSDiscoveryLoadBalancer.java
@@ -1,3 +1,20 @@
+/*
+*
+* Copyright 2013 Netflix, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 package com.netflix.niws.client;
 
 import java.util.HashMap;
@@ -9,20 +26,20 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.netflix.config.DynamicBooleanProperty;
 import com.netflix.config.DynamicDoubleProperty;
 import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.LoadBalancerStats;
-import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.Server;
 import com.netflix.loadbalancer.ZoneSnapshot;
 import com.netflix.servo.monitor.Monitors;
 import com.netflix.servo.monitor.Stopwatch;
 import com.netflix.servo.monitor.Timer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ZoneAwareNIWSDiscoveryLoadBalancer<T extends Server> extends DynamicServerListLoadBalancer<T> {
 
