4fbdadf7c5db056f9f2916bd045c7aa3dc231509
Allen Wang <awang@netflix.com>
2014-04-28 18:43:47 -0700

    Modified examples.

--- ribbon-examples/src/main/java/com/netflix/ribbon/examples/netty/http/LoadBalancingExample.java
+++ ribbon-examples/src/main/java/com/netflix/ribbon/examples/netty/http/LoadBalancingExample.java
@@ -1,14 +1,30 @@
 package com.netflix.ribbon.examples.netty.http;
 
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import io.reactivex.netty.protocol.http.client.HttpClientResponse;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import rx.Observer;
+
+import com.google.common.collect.Lists;
+import com.netflix.client.netty.http.NettyHttpClient;
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.LoadBalancerBuilder;
+import com.netflix.loadbalancer.Server;
+
 
 public class LoadBalancingExample {
-    /*
     public static void main(String[] args) throws Exception {
-        NettyHttpLoadBalancingClient client = NettyHttpClientBuilder.newBuilder()
-                .withFixedServerList(Lists.newArrayList(new Server("www.google.com:80"), new Server("www.microsoft.com:80"), new Server("www.yahoo.com:80")))
-                .build();
-        final CountDownLatch latch = new CountDownLatch(3); 
-        Observer<HttpResponse<ByteBuf>> observer = new Observer<HttpResponse<ByteBuf>>() {
+        List<Server> servers = Lists.newArrayList(new Server("www.google.com:80"), new Server("www.examples.com:80"), new Server("www.wikipedia.org:80"));
+        BaseLoadBalancer lb = LoadBalancerBuilder.newBuilder()
+                .buildFixedServerListLoadBalancer(servers);
+            
+        NettyHttpClient<ByteBuf, ByteBuf> client = NettyHttpClient.createDefaultHttpClient(lb);
+        final CountDownLatch latch = new CountDownLatch(servers.size()); 
+        Observer<HttpClientResponse<ByteBuf>> observer = new Observer<HttpClientResponse<ByteBuf>>() {
             @Override
             public void onCompleted() {
             }
@@ -19,20 +35,16 @@ public class LoadBalancingExample {
             }
 
             @Override
-            public void onNext(HttpResponse<ByteBuf> args) {
+            public void onNext(HttpClientResponse<ByteBuf> args) {
                 latch.countDown();
-                System.err.println("Got response: " + args.getStatus()); 
+                System.out.println("Got response: " + args.getStatus()); 
             }
         };
-        for (int i = 0; i < 3; i++) {
-            // The request is not reusable in RxNetty as its state will be altered. Hence create new 
-            // request for each server before the issue is addressed in RxNetty
-            HttpRequest<ByteBuf> request = HttpRequest.createGet("/");
-            client.createFullHttpResponseObservable(request, null, null).subscribe(observer);
+        for (int i = 0; i < servers.size(); i++) {
+            HttpClientRequest<ByteBuf> request = HttpClientRequest.createGet("/");
+            client.submit(request).subscribe(observer);
         }
         latch.await();        
-        NettyHttpLoadBalancingClient lbClient = (NettyHttpLoadBalancingClient) client;
-        System.out.println(((AbstractLoadBalancer) lbClient.getLoadBalancer()).getLoadBalancerStats());
+        System.out.println(lb.getLoadBalancerStats());
     }
-    */
 }
