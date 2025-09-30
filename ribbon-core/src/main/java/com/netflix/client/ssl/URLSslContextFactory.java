72eb13a1af253929a048b1518b141ae4c6145129
Justin Guerra <jguerra@netflix.com>
2025-09-30 08:57:30 -0600

    migrate to commons-lang3

--- ribbon-core/src/main/java/com/netflix/client/ssl/URLSslContextFactory.java
+++ ribbon-core/src/main/java/com/netflix/client/ssl/URLSslContextFactory.java
@@ -25,7 +25,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- ribbon-loadbalancer/src/main/java/com/netflix/loadbalancer/LoadBalancerContext.java
+++ ribbon-loadbalancer/src/main/java/com/netflix/loadbalancer/LoadBalancerContext.java
@@ -28,7 +28,7 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.servo.monitor.Monitors;
 import com.netflix.servo.monitor.Timer;
 import com.netflix.util.Pair;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
